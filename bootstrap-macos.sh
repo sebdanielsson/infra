62e70e21873345b0e52f546c9afcea5ea00c3cc5
Sebastian <sebastian.danielsson@proton.me>
2023-10-26 16:57:09 +0200

    Add bootstrap script for a fresh macOS installation

---
+++ bootstrap-macos.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Define your repository and target directory
+REPO_URL="https://github.com/sebdanielsson/infra.git"
+TARGET_DIR="$HOME/Git/infra"
+HOMEBREW_PACKAGES=(ansible 1password 1password-cli)
+
+# Clone the infra repo if not cloned
+if [ -d "$TARGET_DIR" ]; then
+    echo "Repository directory already exists. Pulling latest changes."
+    cd "$TARGET_DIR" || exit
+    git pull
+else
+    echo "Cloning repository."
+    git clone "$REPO_URL" "$TARGET_DIR"
+fi
+
+# Install Homebrew if not installed
+if ! command -v brew >/dev/null; then
+    echo "Homebrew is not installed. Installing Homebrew."
+    NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    eval "$(/opt/homebrew/bin/brew shellenv)" >> /Users/sebastian/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+else
+    echo "Homebrew is already installed. Updating Homebrew."
+    brew upgrade
+fi
+
+# Install Homebrew packages
+echo "Installing Homebrew packages."
+brew install "${HOMEBREW_PACKAGES[@]}"
+
+# Start 1Password.app to login
+echo "The script will start 1Password and then exit. Please login to 1Password and enable the 1Password CLI, then run 'op signin' in the terminal."
+read -t 5 -p "Opening 1Password in 10 seconds. Press enter to skip countdown." -r
+open -a 1Password
+exit 0
